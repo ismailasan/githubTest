caafdf1f826edbd554fde1b4b610a5342f4b0e55
ismailasan <ismailas@yahoo.com>
2022-05-17 11:33:45 -0400

    yeniden

--- src/Github.java
+++ src/Github.java
@@ -2,8 +2,7 @@ public class Github {
     public static void main(String[] args) {
         System.out.println("Main branch");
         System.out.println("HEllo world");
-        System.out.println("Main branch");
-        System.out.println("HEllo world");
+
     }
 
 }
